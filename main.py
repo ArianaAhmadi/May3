916d484a69e94af4dd4fc4d44e5104b47adfd9a1
ArianaAhmadi1 <>
2021-05-03 16:57:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#product=0
+#for x in range(1,10+1,1):
+  #for y in range(1,10+1,1):
+    #print("(",x,y,")")
+x=int(input())
+for j in range(1,x+1):
+  for i in range(1,j+1):
+    print(i, end = '')
+print()
